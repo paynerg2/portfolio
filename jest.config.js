d0df4adce1c802bee78beaff5bb586bf4a38ec5e
Rodney Payne <paynerg2@gmail.com>
2022-05-13 14:59:50 -0400

    Add test coverage

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,8 @@ const customJestConfig = {
     setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     moduleDirectories: ['node_modules', '<rootDir>/'],
     testEnvironment: 'jest-environment-jsdom',
+    collectCoverage: true,
+    coverageDirectory: 'jest-coverage',
 };
 
 module.exports = createJestConfig(customJestConfig);

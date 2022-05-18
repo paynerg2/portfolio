66bde2d3735d94265ee17b61aa15b72627a0cbaf
Rodney Payne <paynerg2@gmail.com>
2022-05-17 21:52:49 -0400

    Update filtering to include partial matches

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -2,6 +2,7 @@ import { Button, Stack } from '@chakra-ui/react';
 import { useContext, useEffect, useState } from 'react';
 import { ArrowLeftIcon } from '@chakra-ui/icons';
 import Link from 'next/link';
+import Head from 'next/head';
 import AppContext from '../../utils/appContext';
 import ProjectsSection from '../../components/ProjectsSection/ProjectsSection';
 import SkillsSection from '../../components/SkillsSection/SkillsSection';
@@ -12,27 +13,39 @@ const Projects = () => {
     const [filteredProjects, setFilteredProjects] = useState(projects);
 
     useEffect(() => {
+        // Filter tags by partial string match (i.e. Redux matches Redux and Redux Toolkit)
         setFilteredProjects(
-            projects.filter((project) => selectedTags.every((tag) => project.tags.includes(tag)))
+            projects.filter((project) =>
+                selectedTags.every((tag) => project.tags.some((e) => e.indexOf(tag) >= 0))
+            )
         );
     }, [selectedTags]);
 
     return (
-        <Stack flexDir="column" spacing={10} minH="100vh">
-            <Link href="/" passHref>
-                <Button
-                    w="fit-content"
-                    p={2}
-                    justifySelf="left"
-                    leftIcon={<ArrowLeftIcon />}
-                    variant="ghost"
-                >
-                    Go Back
-                </Button>
-            </Link>
-            <SkillsSection id="skills" />
-            <ProjectsSection projects={filteredProjects} partial={false} />
-        </Stack>
+        <>
+            <Head>
+                <title>Rodney Payne | Projects</title>
+                <meta
+                    name="Projects"
+                    content="An interactive list of a select few of my personal projects."
+                />
+            </Head>
+            <Stack flexDir="column" spacing={10} minH="100vh">
+                <Link href="/" passHref>
+                    <Button
+                        w="fit-content"
+                        p={2}
+                        justifySelf="left"
+                        leftIcon={<ArrowLeftIcon />}
+                        variant="ghost"
+                    >
+                        Go Back
+                    </Button>
+                </Link>
+                <SkillsSection id="skills" />
+                <ProjectsSection projects={filteredProjects} partial={false} />
+            </Stack>
+        </>
     );
 };
 
